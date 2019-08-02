4d3148b394c8d98957c6388d7612b2687c3c074e
Pramod Dhone <pramod.dhone@nitorinfotech.net>
2019-08-03 04:21:14 +0530

    use of Path Variable

--- src/main/java/com/trainingwebservice/resttraining/controller/HelloWorldController.java
+++ src/main/java/com/trainingwebservice/resttraining/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.trainingwebservice.resttraining.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,29 +12,42 @@ import com.trainingwebservice.resttraining.pojo.HelloBean;
 public class HelloWorldController {
 	/**
 	 * using GetMapping
+	 * 
 	 * @return
 	 */
 	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
-	
+
 	/**
 	 * using RequestMapping
+	 * 
 	 * @return
 	 */
 	@RequestMapping(method = RequestMethod.GET, path = "/hello")
 	public String helloWorldWithRequest() {
 		return "Hello";
 	}
-	
+
 	/**
 	 * using GetMapping and with POJO
+	 * 
 	 * @return
 	 */
 	@GetMapping(path = "/hello-bean")
 	public HelloBean helloWorldBean() {
 		return new HelloBean("Pramod", "Welcome to REST API");
+	}
+
+	/**
+	 * with path variable
+	 * 
+	 * @return
+	 */
+	@GetMapping(path = "/hello-bean/{name}")
+	public HelloBean helloWorldBeanWithPath(@PathVariable(name = "name") String username) {
+		return new HelloBean(username, "Welcome to REST API");
 	}
 
 }
